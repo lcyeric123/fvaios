0710b698ed7a48129e79e3633433c18f898dc4ea
lcyeric123 <122712295+lcyeric123@users.noreply.github.com>
2026-07-20 09:17:48 +0800

    Create customize.sh

---
+++ customize.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# customize.sh - run inside chroot
+
+set -e
+
+# repositories
+cat > /etc/apk/repositories << EOF
+https://dl-cdn.alpinelinux.org/alpine/v3.19/main
+https://dl-cdn.alpinelinux.org/alpine/v3.19/community
+EOF
+
+apk update
+
+# install packages from list
+while read -r pkg; do
+    [ -z "$pkg" ] && continue
+    apk add --no-cache "$pkg"
+done < /packages.list
+
+# set hostname
+echo "fvaios" > /etc/hostname
+echo "127.0.0.1 fvaios" >> /etc/hosts
+
+# root auto login on tty1
+sed -i 's|/sbin/getty|/sbin/getty --autologin root|g' /etc/inittab
+
+# enable services
+rc-update add sshd default
+rc-update add nginx default
+rc-update add php-fpm81 default
+
+# build llama.cpp static
+echo "Building llama.cpp ..."
+./build_llama.sh
+
+# generate initramfs for live boot
+echo "Generating initramfs..."
+cat > /etc/mkinitfs/features.d/fvaios.modules << EOF
+kernel/drivers/cdrom
+kernel/fs/isofs
+kernel/fs/squashfs
+kernel/drivers/block/loop.ko
+EOF
+mkinitfs -c /etc/mkinitfs/mkinitfs.conf -b / -o /boot/initramfs-lts $(cat /usr/share/mkinitfs/features.d/../features.list) base squashfs cdrom usb fvaios
+
+# clean up
+rm /packages.list /build_llama.sh /customize.sh
+rm -rf /var/cache/apk/*
